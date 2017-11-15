9a27a0f8e6fb50cffd622deaa7736db91e9ab4be
ykubo1013 <ykubo@se.ritsumei.ac.jp>
2017-11-15 09:05:48 +0900

    あああ

--- app/src/main/java/com/example/ykubo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ykubo/myapplication/MainActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-//masterを変更
+//masterを変更PCから
 
 public class MainActivity extends AppCompatActivity {
 
